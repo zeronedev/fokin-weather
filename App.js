f782de4b6671a669bd1f2f3296dc83bcd480c1dd
ok7375 <ok7375@gmail.com>
2019-12-18 21:06:20 +0900

    1.4 Getting the Weather done (axios)

--- App.js
+++ App.js
@@ -2,17 +2,30 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "c25be38fe43b9517bb955d9d0ec92ead";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you", "So sad");
