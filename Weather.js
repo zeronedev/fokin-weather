541065ce87cdffcd1415d441cb672fab17824275
ok7375 <ok7375@gmail.com>
2019-12-19 22:50:32 +0900

    2.1 Getting the Condition Names done (날씨상태 가져오기)

--- Weather.js
+++ Weather.js
@@ -2,16 +2,26 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import PropTypes from "prop-types";
 
-export default function Weather({ temp }) {
+export default function Weather({ temp, condition }) {
   return (
     <View style={styles.container}>
       <Text>{temp}</Text>
+      <Text>{condition}</Text>
     </View>
   );
 }
 
 Weather.propTypes = {
-  temp: PropTypes.number.isRequired
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds"
+  ]).isRequired
 };
 
 const styles = StyleSheet.create({
