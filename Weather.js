bed4292b930c518d9af13ee2d77d287372c9bbc7
ok7375 <ok7375@gmail.com>
2019-12-22 02:22:39 +0900

    2.3 Background Gradient done (배경 그라데이션)

--- Weather.js
+++ Weather.js
@@ -1,18 +1,70 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const weatherOptions = {
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#3a6186", "#89253e"]
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#108dc7", "#ef8e38"]
+  },
+  Thunderstorm: {
+    iconName: "",
+    gradient: []
+  },
+  Drizzle: {
+    iconName: "",
+    gradient: []
+  },
+  Rain: {
+    iconName: "",
+    gradient: []
+  },
+  Snow: {
+    iconName: "",
+    gradient: []
+  },
+  Atmosphere: {
+    iconName: "",
+    gradient: []
+  },
+  Clear: {
+    iconName: "",
+    gradient: []
+  },
+  Mist: {
+    iconName: "",
+    gradient: []
+  },
+  Haze: {
+    iconName: "",
+    gradient: []
+  }
+};
 
 export default function Weather({ temp, condition }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      style={styles.container}
+      colors={weatherOptions[condition].gradient}
+    >
+      <StatusBar barStyle="light-content" />
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons name="cloud-outline" size={96} color="black" />
+        <MaterialCommunityIcons
+          name={weatherOptions[condition].iconName}
+          size={96}
+          color="white"
+        />
         <Text>{condition}</Text>
-        <Text style={styles.temp}>{temp}o</Text>
+        <Text style={styles.temp}>{temp}℃</Text>
       </View>
       <View style={styles.halfContainer}></View>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -25,6 +77,8 @@ Weather.propTypes = {
     "Snow",
     "Atmosphere",
     "Clear",
+    "Mist",
+    "Haze",
     "Clouds"
   ]).isRequired
 };
@@ -36,7 +90,8 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   temp: {
-    fontSize: 42
+    fontSize: 42,
+    color: "white"
   },
   halfContainer: {
     flex: 1,
